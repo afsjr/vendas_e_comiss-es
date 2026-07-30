fa57b17737a6c317a97ad39bc1feb0aa82cf58d5
AdelinoJR76 <afsjr.bh@gmail.com>
2026-07-30 07:08:47 -0300

    fix: ignore unused event parameter in onAuthStateChange

--- src/components/GlobalAuth.tsx
+++ src/components/GlobalAuth.tsx
@@ -19,7 +19,7 @@ export default function GlobalAuth() {
     };
     checkAuth();
 
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
       setIsAuthenticated(!!session);
     });
 
